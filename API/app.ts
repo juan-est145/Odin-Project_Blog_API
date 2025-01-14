8f8747e717ef20cd33a8429dc922eed257967b2b
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-01-14 18:56:12 +0100

    Added extra packages

--- API/app.ts
+++ API/app.ts
@@ -1,8 +1,12 @@
 import express, { Application } from "express";
 import { router } from "#routes/router";
+import "dotenv/config";
 
 const app: Application = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.disable("x-powered-by");
+
 app.use("/", router);
 
-app.listen(3000, () => console.log("Server listening on port 3000"));
+app.listen(process.env.PORT, () => console.log("Server listening on port 3000"));
