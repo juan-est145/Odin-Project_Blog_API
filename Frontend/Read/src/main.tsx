a85ac62571bad15f490a6f319242e8b6d9430e54
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-01-23 18:42:08 +0100

    Some prop testing with material ui

--- Frontend/Read/src/main.tsx
+++ Frontend/Read/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
+import routes from './Routes.tsx';
+
+const router = createBrowserRouter(routes)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
