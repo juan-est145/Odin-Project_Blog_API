79a59ea7e562461b3d58268551c217bda25267e8
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-01-27 20:07:15 +0100

    We have a centered log in form

--- Frontend/Read/src/components/AccountPage.tsx
+++ Frontend/Read/src/components/AccountPage.tsx
@@ -1,9 +1,28 @@
+import { Button } from "primereact/button";
+import { Card } from "primereact/card";
+import { FloatLabel } from "primereact/floatlabel";
+import { InputText } from "primereact/inputtext";
+import { Password } from "primereact/password";
 
 
 export function LogIn() {
 	return (
 		<>
-			<h1>Nothing again in log in</h1>
+			<main className="flex min-h-screen flex-column justify-content-center align-items-center">
+				<Card title={<h3 style={{ margin: 0, textAlign: "center" }}>Log in</h3>}>
+					<div className="flex flex-column gap-4">
+						<FloatLabel>
+							<InputText id={"username"} />
+							<label htmlFor="username">Username</label>
+						</FloatLabel>
+						<FloatLabel>
+							<Password inputId="password" toggleMask feedback={false}/>
+							<label htmlFor="password">Password</label>
+						</FloatLabel>
+						<Button className="align-self-center">Log in</Button>
+					</div>
+				</Card>
+			</main>
 		</>
 	);
 }
