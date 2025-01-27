7fd6f18cff47001c1cc3792619ed9768dc0add46
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-01-27 20:18:24 +0100

    Username is now same width that password

--- Frontend/Read/src/components/AccountPage.tsx
+++ Frontend/Read/src/components/AccountPage.tsx
@@ -8,15 +8,15 @@ import { Password } from "primereact/password";
 export function LogIn() {
 	return (
 		<>
-			<main className="flex min-h-screen flex-column justify-content-center align-items-center">
+			<main className="flex min-h-screen flex-column justify-content-center align-items-center bg-gray-700">
 				<Card title={<h3 style={{ margin: 0, textAlign: "center" }}>Log in</h3>}>
 					<div className="flex flex-column gap-4">
 						<FloatLabel>
-							<InputText id={"username"} />
+							<InputText className="min-w-full" id={"username"} />
 							<label htmlFor="username">Username</label>
 						</FloatLabel>
 						<FloatLabel>
-							<Password inputId="password" toggleMask feedback={false}/>
+							<Password className="min-w-full" inputId="password" toggleMask feedback={false}/>
 							<label htmlFor="password">Password</label>
 						</FloatLabel>
 						<Button className="align-self-center">Log in</Button>
