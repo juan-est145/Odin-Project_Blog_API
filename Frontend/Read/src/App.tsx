aecda04a2c18530b7ab9db1e949f93c69336666c
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-01-27 15:06:17 +0100

    Fresh restart

--- Frontend/Read/src/App.tsx
+++ Frontend/Read/src/App.tsx
@@ -1,39 +1,13 @@
-import {
-  Theme,
-  ThemePanel,
-  ThemeProps,
-} from "@radix-ui/themes";
-
-import { RouterProvider } from 'react-router-dom';
-import { createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import routes from './Routes.tsx';
-import { useState } from "react";
-import { IThemeContextChild } from "#types/types.ts";
-import { ThemeProvider } from "#context/context.tsx";
 
 const router = createBrowserRouter(routes);
 
 export default function App() {
-  const [appearance, setAppearance] = useState<ThemeProps['appearance']>("dark");
-  const [color] = useState<ThemeProps["accentColor"]>("teal");
   // TO DO: Try with useEffect to store appearance preference in local storage
   return (
     <>
-      <Theme accentColor={color} grayColor="olive" scaling="95%" appearance={appearance}>
-        <ThemeContext appearance={appearance} setAppearance={setAppearance} accentColor={color}>
-          <RouterProvider router={router}></RouterProvider>
-        </ThemeContext>
-        <ThemePanel />
-      </Theme>
+      <RouterProvider router={router}></RouterProvider>
     </>
-  );
-
-}
-
-function ThemeContext({ children, appearance, setAppearance, accentColor }: IThemeContextChild) {
-  return (
-    <ThemeProvider.Provider value={{ appearance, setAppearance, accentColor }}>
-      {children}
-    </ThemeProvider.Provider>
   );
 }
