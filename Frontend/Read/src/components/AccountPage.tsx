7c6a64acf2c8b1752dbd53dcfabb5557c9a91354
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-02-11 10:37:56 +0100

    We now display error messages in login page

--- Frontend/Read/src/components/AccountPage.tsx
+++ Frontend/Read/src/components/AccountPage.tsx
@@ -1,10 +1,11 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 import { Button } from "primereact/button";
 import { Card } from "primereact/card";
 import { FloatLabel } from "primereact/floatlabel";
 import { InputText } from "primereact/inputtext";
+import { Toast } from "primereact/toast";
 import { Password } from "primereact/password";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 // TO DO: For input validation use Toast
@@ -13,6 +14,7 @@ export function LogIn() {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 	const redirect = useNavigate();
+	const toast = useRef<Toast>(null);
 
 	async function postLogin() {
 		try {
@@ -25,7 +27,13 @@ export function LogIn() {
 			localStorage.setItem("jwt", token.data);
 			redirect("/");
 		} catch (error) {
-			// Temporal meausre
+			if (error instanceof AxiosError) {
+				if (error.response) {
+					return toast.current?.show({ severity: "error", summary: "Error", detail: error.response.data });
+				}
+				else if (error.request)
+					return toast.current?.show({ severity: "error", summary: "Error", detail: "Something went wrong, please try at a later time" })
+			}
 			console.error(error);
 		}
 	}
@@ -35,6 +43,7 @@ export function LogIn() {
 			<main className="flex min-h-screen flex-column justify-content-center align-items-center bg-gray-700">
 				<Card title={<h3 style={{ margin: 0, textAlign: "center" }}>Log in</h3>}>
 					<div className="flex flex-column gap-4">
+						<Toast ref={toast} />
 						<FloatLabel>
 							<InputText
 								name={"username"}
