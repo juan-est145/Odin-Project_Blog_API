7711474490e6001be96a5b9f8dd49801a928bfca
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-03-16 13:25:15 +0100

    We have post requests too

--- nest/src/cats/cats.controller.ts
+++ nest/src/cats/cats.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Param, Post, Req, Res } from "@nestjs/common";
+import { Response } from "express";
 
 @Controller("cats")
 export class CatsController {
   @Get()
-  findAll(): string {
-    return "This action returns all cats";
+  findAll(@Req() request: Request): string {
+    return `This action returns all cats. Hello from ${request.url}`;
   }
   @Get("/oneCat")
   findOneCat(): string {
     return "You found one specific cat";
+  }
+  @Get("/oneCat/:id")
+  findCatId(@Param("id") param: number): string {
+    return `Your cat id is ${param}`;
+  }
+  @Post()
+  create(@Res() res: Response): void {
+    res.status(201).send("This action creates a cat");
   }
 }
