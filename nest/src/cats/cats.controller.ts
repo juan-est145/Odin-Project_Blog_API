9750d14ef9e2fc27f38afb112380af7642618feb
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-03-16 12:48:59 +0100

    cats controller added

---
+++ nest/src/cats/cats.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from "@nestjs/common";
+
+@Controller("cats")
+export class CatsController {
+  @Get()
+  findAll(): string {
+    return "This action returns all cats";
+  }
+  @Get("/oneCat")
+  findOneCat(): string {
+    return "You found one specific cat";
+  }
+}
