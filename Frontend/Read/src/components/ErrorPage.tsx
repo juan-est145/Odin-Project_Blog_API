f0217387a59cb012d501256f0d968c873afb2ff2
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-03-24 19:58:39 +0100

    Increased the size of the font and added a favicon

--- Frontend/Read/src/components/ErrorPage.tsx
+++ Frontend/Read/src/components/ErrorPage.tsx
@@ -8,10 +8,10 @@ export function ErrorPage() {
 		<>
 			<Card>
 				<main className="flex justify-center align-items-center flex-column">
-					<h1 className="text-6xl font-bold text-primary align-self-start m-0">Oops</h1>
+					<h1 className="text-6xl font-bold text-primary align-self-start m-0">Oops !!!</h1>
 					<Divider/>
 					<img src={shrekGif} alt="Shrek gif"/>
-					<h3>It seems you got lost. You can go back to the main menu by clicking <Link className="text-primary" to={"/"}>here</Link></h3>
+					<h3 className="text-3xl">It seems you got lost. You can go back to the main menu by clicking <Link className="text-primary" to={"/"}>here</Link></h3>
 				</main>
 			</Card>
 		</>
