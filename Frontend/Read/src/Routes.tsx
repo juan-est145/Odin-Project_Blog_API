6ea13c45ccfeff7ccb32d98a0e68f79a2519b912
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-03-24 19:25:52 +0100

    Sketched a 404 error page

--- Frontend/Read/src/Routes.tsx
+++ Frontend/Read/src/Routes.tsx
@@ -2,11 +2,13 @@ import { RouteObject } from "react-router-dom";
 import MainPage from "#project/src/components/MainPage";
 import { LogIn, SignIn } from "#project/src/components/AccountPage";
 import PostPage from "#project/src/components/PostPage";
+import { ErrorPage } from "./components/ErrorPage";
 
 const routes: RouteObject[] = [
 	{
 		path: "/",
 		element: <MainPage/>,
+		errorElement: <ErrorPage/>,
 	},
 	{
 		path: "/log-in",
---
+++ Frontend/Read/src/components/ErrorPage.tsx
@@ -0,0 +1,19 @@
+import { Card } from "primereact/card";
+import { Divider } from "primereact/divider";
+import { Link } from "react-router-dom";
+import shrekGif from "#project/src/assets/shrek-reaction.gif"
+
+export function ErrorPage() {
+	return (
+		<>
+			<Card>
+				<main className="flex justify-center align-items-center flex-column">
+					<h1 className="text-6xl font-bold text-primary align-self-start m-0">Oops</h1>
+					<Divider/>
+					<img src={shrekGif} alt="Shrek gif"/>
+					<h3>It seems you got lost. You can go back to the main menu by clicking <Link className="text-primary" to={"/"}>here</Link></h3>
+				</main>
+			</Card>
+		</>
+	);
+}
