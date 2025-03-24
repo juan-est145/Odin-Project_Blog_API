bdb2a1c1c34872b28e451f35e6f0b1d3b1fad26d
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-03-24 21:24:44 +0100

    Added some style to post page

--- Frontend/Read/src/components/PostPage.tsx
+++ Frontend/Read/src/components/PostPage.tsx
@@ -2,17 +2,19 @@ import { useParams } from "react-router-dom";
 import { Header } from "./MainPage";
 import { Card } from "primereact/card";
 import { Divider } from "primereact/divider";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "#project/src/Context";
 import apiClient from "../ApiClient";
+import { Posts } from "../types/types";
 
 export default function PostPage() {
 	const { loggedIn } = useAuth();
 	const { postId } = useParams<string>();
+	const [post, setPosts] = useState<Posts | undefined>(undefined);
 
 	useEffect(() => {
 		const promise = apiClient.GET("/v1/posts/{id}", { params: { path: { id: postId as string } } })
-		promise.then((value) => console.log(value))
+		promise.then((value) => setPosts(value.data))
 			.catch(() => alert("Something went wrong"));
 	}, [postId, loggedIn]);
 
@@ -20,10 +22,22 @@ export default function PostPage() {
 		<>
 			<Header></Header>
 			<Divider />
-			<Card>
-				<h1>Esto ha tirado, estamos en {postId}</h1>
-			</Card>
+			<ContentArea post={post}></ContentArea>
+			<Divider />
+		</>
+	);
+}
 
+function ContentArea({ post }: { post: Posts | undefined }) {
+	return (
+		<>
+			<Card className="p-4">
+				<main className="flex flex-column">
+					<h1 className="text-primary underline text-5xl m-0">{post?.title}</h1>
+					<h3 className="text-3xl">{post?.subtitle}</h3>
+					<p className="align-self-center text-center 70" style={{ width: "80ch" }}>{post?.text}</p>
+				</main>
+			</Card>
 		</>
 	);
 }
