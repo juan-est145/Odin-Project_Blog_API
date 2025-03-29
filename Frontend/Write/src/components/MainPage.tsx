d6797cc1b96256c7997e2de25e4e27427efe6d54
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-03-29 16:40:33 +0100

    Added new tab menu

--- Frontend/Write/src/components/MainPage.tsx
+++ Frontend/Write/src/components/MainPage.tsx
@@ -1,10 +1,19 @@
 import { useAuth } from "#project/src/Context";
+import { MenuItem } from "primereact/menuitem";
+import { TabMenu } from "primereact/tabmenu";
 
 export default function MainPage() {
+	const items: MenuItem[] = [
+		{ label: "Your comments" },
+		{ label: "Your posts" },
+		{ label: "Create a new post" },
+	];
+
 	const { loggedIn } = useAuth();
 	return (
 		<>
 			{loggedIn ? <h1>You are logged in</h1> : <h1>You are not logged in</h1>}
+			<TabMenu model={items}></TabMenu>
 		</>
 	)
 }
