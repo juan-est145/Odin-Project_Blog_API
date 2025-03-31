f9d2746d9eb884662e9bac23c25ff306e17a088e
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-03-31 21:22:06 +0200

    Created Post Creator component

--- Frontend/Write/src/components/PostCreator.tsx
+++ Frontend/Write/src/components/PostCreator.tsx
@@ -1,7 +1,27 @@
+import { Divider } from "primereact/divider";
+import { Editor } from "primereact/editor";
+import { MenuItem } from "primereact/menuitem";
+import { SplitButton } from "primereact/splitbutton";
+import { useState } from "react";
+
 export function PostCreator() {
+	const [text, setText] = useState<string>("");
+
+	const items: MenuItem[] = [
+		{ label: "Publish", icon: "pi pi-check", command: () => alert("Nothing yet") }
+	];
+
 	return (
 		<>
-			<h1>Here we are</h1>
+			<Editor
+			value={text}
+			onTextChange={(e) => setText(e.textValue)}
+			style={{ minHeight: "300px" }}></Editor>
+			<Divider/>
+			<SplitButton 
+			icon={"pi pi-save"} 
+			label={"Save"}
+			model={items}></SplitButton>
 		</>
 	);
 }
