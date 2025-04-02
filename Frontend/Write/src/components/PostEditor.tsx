c5073faa7575123694959ac4181d3d7cd968ac50
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-04-02 18:46:10 +0200

    Decorated the your posts collection

--- Frontend/Write/src/components/PostEditor.tsx
+++ Frontend/Write/src/components/PostEditor.tsx
@@ -1,6 +1,8 @@
 import { Posts } from "../types/types";
 import apiClient from "../APiClient";
 import { useEffect, useState } from "react";
+import { Fieldset } from "primereact/fieldset";
+import { Divider } from "primereact/divider";
 
 export function PostEditor() {
 	const [posts, setPosts] = useState<Posts[]>([]);
@@ -22,12 +24,14 @@ export function PostEditor() {
 function Post({ data }: { data: Posts }) {
 	return (
 		<>
-			<h2>{data.title}</h2>
-			{data.subtitle ? <h3>{data.subtitle}</h3> : null}
-			<p>{data.text}</p>
-			<p>Created at: {data.createdAt}</p>
-			<p>Updated at: {data.updatedAt}</p>
-			<p>Status: {data.published ? "Published" : "Not published"}</p>
+			<Fieldset legend={data.title} toggleable>
+				{data.subtitle ? <h3>{data.subtitle}</h3> : null}
+				<p>{data.text}</p>
+				<p><b className="underline">Created at:</b> {data.createdAt}</p>
+				<p><b className="underline">Updated at:</b> {data.updatedAt}</p>
+				<p><b className="underline">Status:</b> {data.published ? "Published" : "Not published"}</p>
+			</Fieldset>
+			<Divider/>
 		</>
 	);
 }
